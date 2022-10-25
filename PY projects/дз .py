5f5b6101b5fb17a9efd427841630694247d9db06
Alex <114764888+PYninjaAlex@users.noreply.github.com>
2022-10-25 20:29:28 +0300

    My lessons continue I will post all my works.These works are not a full projects!

--- PY projects/дз .py
+++ PY projects/дз .py
@@ -100,15 +100,29 @@
 
 
 
-n = int(input(' Введите количество элементов списка '))
-list1 = []
-for i in range(n):
-    a = int(input(f' Введите число {i + 1} '))
-    list1.append(a)
-b = 0
-for a in list1:
-    b += a
-f = b /  n
-print(f)
+# n = int(input(' Введите количество элементов списка '))
+# list1 = []
+# for i in range(n):
+#     a = int(input(f' Введите число {i + 1} '))
+#     list1.append(a)
+# b = 0
+# for a in list1:
+#     b += a
+# f = b /  n
+# print(f)
 
 
+
+
+
+
+
+s = input(' Введите текст ')
+res =""
+for i in range(len(s)):
+    a = s[i]
+    b = ord('a')
+    b += 13
+    c = chr(b)
+    res += c
+print(res)
